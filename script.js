e28b11a3d2791b3ee8b0202ed2ea3f950ebaa83d
Braggsz <egfdbhudhwygtgwff381@gmail.com>
2024-10-19 11:51:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
-// Получаем параметр ref из URL
-const urlParams = new URLSearchParams(window.location.search);
-const ref = urlParams.get('ref');
+<script>
+    // Получаем параметр реферальной ссылки из URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const ref = urlParams.get('ref');
 
-if (ref) {
-    // Если ref существует, отправляем запрос в Telegram бота
-    fetch(`https://api.telegram.org/bot7039961825:AAGmyb-lFESj2INpi81JWQAV9kgZwPBtJSU/sendMessage`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            chat_id: ref,  // Отправляем сообщение пользователю с этим chat_id
-            text: "Кто-то перешел по вашей реферальной ссылке!"
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log("Сообщение отправлено:", data);
-    })
-    .catch(error => {
-        console.error("Ошибка при отправке сообщения:", error);
-    });
-}
+    if (ref) {
+        // Отправляем реферальный ID на сервер или сохраняем его
+        console.log("Реферальная ссылка активна, ID реферера: " + ref);
+
+        // Отправьте запрос на сервер для уведомления о переходе по реферальной ссылке
+        fetch(`https://ваш_сервер/track_referral?ref=${ref}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Реферальный переход зафиксирован");
+            });
+    }
+</script>
